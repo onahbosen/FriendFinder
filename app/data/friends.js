db0704e47d2be51809da97f6d028f6b245661fa4
onahbosen <onahbosen@gmail.com>
2019-04-24 17:00:14 -0500

    all done

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friendsArray = [
+    {
+        name: 'Freckles',
+        photo: 'https://ofsds.org/wp-content/uploads/2019/04/Freckles.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Mac',
+        photo: 'https://ofsds.org/wp-content/uploads/2019/03/Mac.png',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Bobby',
+        photo: 'https://ofsds.org/wp-content/uploads/2019/01/Bobby-Blind-Dog-Old-Friends-Senior-Dog-Sanctuary.jpg',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Dallas',
+        photo: 'https://ofsds.org/wp-content/uploads/2019/01/Dallas-Blind-Dog-Old-Friends-Senior-Dog-Sanctuary.jpg',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Beau',
+        photo: 'https://ofsds.org/wp-content/uploads/2019/03/Beau.png',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
